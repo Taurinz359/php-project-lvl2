8bedb692ed09c48caaa9daf7f642ee32b1abed93
Gadel <gadel.g@myhart.ru>
2022-07-29 10:04:03 +0300

    [3] ver 1

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -18,6 +18,9 @@ genDiff(__DIR__ . '/../tests/file1.json', __DIR__ . '/../tests/file2.json', '');
 function genDiff(string $firstFile, string $secondFile, string $format): string
 {
     [$firstFileContent, $secondFileContent] = getValidateFileContent($firstFile, $secondFile);
+    ksort($firstFileContent);
+    ksort($secondFileContent);
+    generateStruct($firstFileContent, $secondFileContent);
 }
 
 /**
@@ -35,4 +38,19 @@ function getValidateFileContent(string ...$files): array|Exception
         $content[] = json_decode(file_get_contents($file), true);
     }
     return $content;
+}
+
+function generateStruct(array $firstFile, array $secondFile): array
+{
+    $struct = array_reduce($firstFile, function ($acc, $item) use ($firstFile, $secondFile) {
+        $key = array_search($item, $firstFile);
+        $acc[] = [
+            'key' => $key,
+            'firstValue' => array_key_exists($key, $firstFile) ? $item : null,
+            'secondValue' => array_key_exists($key, $secondFile) ? $secondFile[$key] : null,
+        ];
+        return $acc;
+    });
+
+    var_dump($struct);
 }
